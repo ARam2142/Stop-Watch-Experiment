25fce3fa5414fb7a8bd42e7c3bcb63dc790bb520
Aram2142 <andresaaramirez@aol.com>
2021-02-13 17:49:12 -0500

    buttons now functional

--- index.js
+++ index.js
@@ -1,49 +1,71 @@
-//create stop watch timer
-//create functionality for start stop and reset buttons
-//use set Interval clear in interval
-//format time in hh:mm:ss
-//use date object to set a time and find 
-//plan for 4 functions, function startTime(), function pause(), function resetTime(), function displayTime()
+window.onload = function () {
+    //grab possible elements needed
+    let timerEl = document.getElementById("timer-text")
+    let startBtn = document.getElementById("start")
+    let restartBtn = document.getElementById("restart");
+    let stopBtn = document.getElementById('stop');
 
-//grab possible elements needed
-var timerEl = document.getElementById("timer-text")
-var startBtn = document.getElementById("start")
-var restartBtn = document.getElementById("restart");
-var stopBtn = document.getElementById("stop");
+    //hold variables of time and set to 0
+    let hours = parseInt('0');
+    let minutes = parseInt('0');
+    let seconds = parseInt('0');
+    //let tenth = 0
 
-//hold variables of time and set to 0
-let hours = parseInt('00');
-let minutes = parseInt('00');
-let seconds = parseInt('00');
-let tenth = 0
-//let milliseconds =0;
+    //timer
+    function timer() {
+        seconds++
+        //console.log(seconds)
+        if (seconds == 60) {
+            minutes++
+            seconds = 0;
+            hours = 0
+        }
+        if (minutes == 60) {
+            hours++
+            minutes = 0;
+            hours = 0;
+        }
 
-//timer
-function timer() {
-    seconds++
-    //console.log(seconds)
-    if (seconds == 60) {
-        minutes++
-        seconds = 0;
-        hours = 0
+        if (seconds < 10) {
+            
+        }
+
+        timerEl.textContent = hours + ": " + minutes + ": " + seconds;
+
     }
-    if (minutes == 60) {
-        hours++
-        minutes = 0;
-        hours = 0;        
-    }
-    
-    timerEl.textContent = hours + ": " + minutes + ": " + seconds;
 
-    if(seconds < 10) {
-        return "0" + seconds
+    //interval IMPORTANT
+    let runTheClock;
+
+    //timer is running
+    function runTimer() {
+        runTheClock = setInterval(timer, 30);;
     }
-    
+
+    //function that will stop timer
+    function stopTimer() {
+        clearInterval(runTheClock)
+    }
+
+    //function will reset timer
+    function resetTimer() {
+        stopTimer();
+        return timerEl.textContent = "00:00:00"
+    }
+
+    restartBtn.addEventListener("click", function () {
+        resetTimer();
+    })
+
+    //button will pause the timer
+    stopBtn.addEventListener("click", function () {
+        stopTimer();
+    })
+
+    //button will start the timer
+    startBtn.addEventListener("click", function () {
+        runTimer();
+    })
+
+
 }
-
-function timerRunning() {
-    var t 
-    t = setInterval(timer, 1000);
-    console.log(t)
-}
-timerRunning()
